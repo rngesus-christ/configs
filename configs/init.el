b9173d54c6966b20bb47f9175081b07bb7c3c688
rngesus-christ <tavo8477@gmail.com>
2018-08-23 16:24:41 -0400

    Add existing file

---
+++ configs/init.el
@@ -0,0 +1,37 @@
+
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#2d3743" "#ff4242" "#74af68" "#dbdb95" "#34cae2" "#008b8b" "#00ede1" "#e1e1e0"])
+ '(custom-enabled-themes (quote (deeper-blue)))
+ '(org-agenda-files nil)
+ '(package-selected-packages
+   (quote
+    (ac-haskell ac-haskell-process emojify ac-emoji auto-complete company-mode pdf-tools rainbow-mode avy htmlize smex ido-vertical-mode org-mode evil-mode haskell-mode org-link-minor-mode evil))))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 135 :width normal :foundry "Bits" :family "Liberation Mono")))))
+
+
+
+
